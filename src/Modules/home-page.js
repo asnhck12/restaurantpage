c6e962b867eeda764a57232c8707db0f820852f8
AH LT <ahussain93@live.co.uk>
2023-04-11 17:35:55 +0100

    All functions and basic structure complete

--- src/Modules/home-page.js
+++ src/Modules/home-page.js
@@ -1,20 +1,26 @@
 function loadHomePage() {
     const contentBody = document.getElementById('content');
-    const title = document.createElement('h2');
+    const title = document.createElement('div');
+    const titleSection = document.createElement('h2');
     title.setAttribute('id','title');
-    title.innerHTML = "My Restaurant";
+    titleSection.innerHTML = "My Restaurant";
 
+    const mainImageSection = document.createElement('div');
     const mainImage = document.createElement('img');
-    mainImage.setAttribute('id','mainImage');
+    mainImageSection.setAttribute('id','mainImage');
     mainImage.src = "../assets/Restaurant small.jpg";
 
+    const aboutSection = document.createElement('div');
     const about = document.createElement('p');
-    about.setAttribute('id','about');
+    aboutSection.setAttribute('id','about');
     about.innerHTML = "Welcome to our restaurant, a place with a warm atmosphere and a wide variety of delicious food and drink.";
 
     contentBody.appendChild(title);
-    contentBody.appendChild(mainImage);
-    contentBody.appendChild(about);
+    title.appendChild(titleSection);
+    contentBody.appendChild(mainImageSection);
+    mainImageSection.appendChild(mainImage);
+    contentBody.appendChild(aboutSection);
+    aboutSection.appendChild(about);
 
     };
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,36 @@ import { loadHomePage } from './Modules/home-page';
 import { menu } from './Modules/menu';
 import { contactPage } from './Modules/contact-page';
 
-// window.addEventListener('load', loadHomePage);
-// window.addEventListener('load', menu);
-window.addEventListener('load', contactPage);
+const contentBody = document.getElementById('content');
+const tabSection = document.createElement('div');
+const homeButton = document.createElement('button');
+const menuButton = document.createElement('button');
+const contactButton = document.createElement('button');
+
+function loadMenu() {
+    clearPage();
+    tabSection.setAttribute('id','menuHeader');
+    homeButton.innerHTML = "Home";
+    menuButton.innerHTML = "Menu";
+    contactButton.innerHTML = "Contact Us";
+
+    tabSection.setAttribute('id','tabSection');
+
+    contentBody.appendChild(tabSection);
+    tabSection.appendChild(homeButton);
+    tabSection.appendChild(menuButton);
+    tabSection.appendChild(contactButton);
+}
+
+function clearPage() {
+    contentBody.innerHTML = '';
+}
+
+window.addEventListener('load', loadMenu);
+window.addEventListener('load', loadHomePage);
+homeButton.addEventListener('click', loadMenu);
+homeButton.addEventListener('click', loadHomePage);
+menuButton.addEventListener('click', loadMenu);
+menuButton.addEventListener('click', menu);
+contactButton.addEventListener('click', loadMenu);
+contactButton.addEventListener('click', contactPage);
